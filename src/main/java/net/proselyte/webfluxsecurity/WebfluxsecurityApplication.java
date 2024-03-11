a3fbf37fc86973a0f76aa413f2ce947298717c3d
DESKTOP-9TABSRA\Farhad.Ahmedov <Farhad.ahmadov@eurodesign.az>
2024-03-11 17:24:46 +0400

    create project

---
+++ src/main/java/net/proselyte/webfluxsecurity/WebfluxsecurityApplication.java
@@ -0,0 +1,13 @@
+package net.proselyte.webfluxsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxsecurityApplication.class, args);
+    }
+
+}
